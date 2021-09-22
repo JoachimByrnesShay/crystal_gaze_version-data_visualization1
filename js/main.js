ed31be2ef319b77628320cc36bd7a82ef664f9e1
JoachimByrnesShay <me@joachimbyrnesshay.com>
2021-09-22 00:54:07 -0400

    graphs of relative currency values are displaying proportionally to the currency which rates at the highest multiple of base currency, ie if 1usd == 85afn, then 85.46afn is 100% height and 1usd is (100 / 85.46 * 1)% height;  basic custom modal is working; flash warning message added to header when user tries to select more than 4 comparison currencies; flash warning message currently disappears when user deselects a currency- this will be tweake; graphs re-render upon dynamic change of base currency; functionality of deletion of base currency from comparisons if previously a comparison fixed; error fixed with splicing of comparison currencies array so that only correct element is removed

--- js/main.js
+++ js/main.js
@@ -4,7 +4,35 @@ let requestURL = 'https://api.exchangerate.host/';
 let currency = { convertFrom: 'USD', convertTo: [], data: {} };
 
 
+function makeModalDiv(classStr) {
+    let div = document.createElement('div');
+    let button = document.createElement('button');
+    // button.style.margin = '2em';
+    div.textContent = `${classStr}
+    ${currency.data.rates[classStr]}`;
+    // button.textContent = 'hi';
+    div.classList.add('modal', classStr);
+    // button.classList.add('modal-btn');
+    // div.appendChild(button);
 
+    return div;
+
+}
+
+function showModal(div) {
+
+    div.style.display = "block"; //this is the replace of this line
+
+    div.onmouseout = function() {
+        // Do your delete operation
+        div.style.display = 'none';
+    };
+
+}
+
+fetchAPIData();
+
+/* fetch functions */
 function fetchCurrencyData() {
     let requestedURL = `${requestURL}latest?base=${currency.convertFrom}`
     return fetch(requestedURL).
@@ -20,31 +48,49 @@ function fetchSymbols() {
     return fetch(requestedURL).
     then(response => response.json()).
     then((symbolData) => {
-        currency.symbols = symbolData.symbols
+        currency.symbols = symbolData.symbols;
         return;
     }).catch(error => console.warn(error));
 }
 
 function fetchAPIData() {
     fetchCurrencyData().
-    then(() => {
-        console.log(currency.data.rates)
-    }).then(() => fetchSymbols()).then(() => render());
+    then(() => fetchSymbols()).then(() => render());
 }
 
 function changeBase() {
-    let select = document.querySelector('.section-config-base select')
+    // THINK CAREFULLY ABOUT THIS, REVISIT
+    let select = document.querySelector('.section-config-base select');
     let selected = select.options[select.selectedIndex].value;
-    //console.log(select.options[select.selectedIndex].value)
-    currency.convertFrom = select.options[select.selectedIndex].value.slice(0, 3)
-    console.log(selected);
-    console.log(currency.convertTo);
-    console.log(currency.convertTo.indexOf(selected));
-    currency.convertTo.splice(currency.convertTo.indexOf(currency.convertFrom), 1);
+    currency.convertFrom = select.options[select.selectedIndex].value.slice(0, 3);
 
+    //check explicitly, because splice argument one will evaluate to -1 if not present
+    // splice is defined to treat -1 index as last element
+    if (currency.convertTo.includes(currency.convertFrom)) {
+        currency.convertTo.splice(currency.convertTo.indexOf(currency.convertFrom), 1);
+    }
     render();
+}
+
+function addFlashError() {
+    let warningElem = document.querySelector('.header-warning');
+
+    let message = "You cannot select more than 4 comparisons!  Deselect one (with click), then select another.";
+
+    warningElem.classList.add('warning-added');
+    warningElem.textContent = message;
 
 }
+
+function removeFlashError() {
+    let warningElem = document.querySelector('.header-warning');
+    warningElem.classList.remove('warning-added');
+    warningElem.textContent = '';
+    render();
+}
+
+
+/* page configuration functions */
 
 function makeComparisonCurrencyEventListener(thisOption, rate) {
     thisOption.addEventListener('mousedown', function(e) {
@@ -52,9 +98,16 @@ function makeComparisonCurrencyEventListener(thisOption, rate) {
         if (e.target.selected) {
             e.target.selected = false;
             currency.convertTo.splice(currency.convertTo.indexOf(rate), 1);
+            removeFlashError();
+            render();
         } else if (currency.convertTo.length <= 3) {
             currency.convertTo.push(rate);
             e.target.selected = true;
+
+
+        } else {
+            console.log("You cannot select more than 4 comparisons!");
+            addFlashError();
         }
         showConfigurationSelectionsPanel();
     });
@@ -66,7 +119,7 @@ function showConfigurationSelectionsPanel() {
     baseShow.textContent = currency.convertFrom;
     let comparisonShow = document.querySelector('.section-config-show-comparisons');
     let comparisonUL = document.createElement('ul');
-    comparisonShow.innerHTML = ''
+    comparisonShow.innerHTML = '';
     comparisonShow.appendChild(comparisonUL);
 
     for (let comparison of currency.convertTo) {
@@ -80,6 +133,7 @@ function makeBaseCurrencySelectionOption(thisSelect, rate) {
     let option = document.createElement('option');
     let rateName = currency.symbols[rate].description;
     option.textContent = `${rate}: ${rateName}`;
+    // TAKE A LOOK, SHOULD THIS BE A SEPARATE FUNCTION FOR THE BELOW SCENARIO
     if (rate === currency.convertFrom) option.selected = true;
     thisSelect.appendChild(option);
 
@@ -92,6 +146,7 @@ function makeComparisonCurrencySelectionOption(thisSelect, rate) {
         option.textContent = `${rate}: ${rateName}`;
         thisSelect.appendChild(option);
         makeComparisonCurrencyEventListener(option, rate);
+        // TAKE A SECOND LOOK AT WHAT I AM DOING HERE, CAN THIS BE A SEPARATE CONCERN
         if (currency.convertTo.includes(rate)) {
             option.setAttribute('selected', true);
         } else {
@@ -103,14 +158,79 @@ function makeComparisonCurrencySelectionOption(thisSelect, rate) {
     }
 }
 
+function charts() {
+    console.log('make charts');
+    let graphArea = document.querySelector('section-graphs');
+    // graphArea.innerHTML = ''
+    let toMakeGraphsFrom = currency.convertTo;
+    let graphableCurrencies = {}
+    graphableCurrencies[currency.convertFrom] = 1;
+    let maxHeight = 1;
+    for (let thisCurrency of currency.convertTo) {
+        let val = (Number(currency.data.rates[thisCurrency]));
+        graphableCurrencies[thisCurrency] = val;
+        console.log(val);
+        if (val > maxHeight) {
+            maxHeight = val;
+        }
+
+
+    }
+    console.log(maxHeight);
+    let ratio = 100 / maxHeight;
+    console.log('ratio', ratio);
+    //maxHeight =
+    console.log(graphableCurrencies);
+    let titleRow = document.querySelector('.graphs-title');
+    let graphsRow = document.querySelector('.graphs-graphing');
+    titleRow.innerHTML = '';
+    graphsRow.innerHTML = '';
+    for (let graphName in graphableCurrencies) {
+        let height = ratio * Number(graphableCurrencies[graphName]);
+
+        let titleElement = document.createElement('p');
+        titleElement.innerHTML = graphName;
+        titleRow.appendChild(titleElement);
+        console.log(titleElement);
+
+        let graphElement = document.createElement('div');
+
+        let modal = makeModalDiv(graphName);
+
+        titleElement.appendChild(modal);
+
+        graphElement.onmouseover = function() {
+
+            showModal(modal);
+        }
+        graphElement.onmouseout = function() {
+            modal.style.display = 'none';
+        }
+        titleElement.onmouseover = graphElement.onmouseover;
+        titleElement.onmouseout = graphElement.onmouseout;
+        graphElement.style.height = height + '%';
+
+        graphElement.style.minHeight = '10px';
+        graphElement.style.minWidth = '50px';
+        graphElement.style.background = 'red';
+        // graphElement.textContent = 'g';
+        graphsRow.appendChild(graphElement);
+    }
+}
+
+
+/* render */
+
 function render() {
     showConfigurationSelectionsPanel();
-    console.log('rendering')
+    console.log('rendering');
+    console.log(currency);
+
     let baseSelect = document.querySelector('.section-config-base select');
     let convertSelect = document.querySelector('.section-config-conversions select');
     //clears dom of comparison select options
-    convertSelect.innerHTML = ''
-    baseSelect.innerHTML = ''
+    convertSelect.innerHTML = '';
+    baseSelect.innerHTML = '';
     console.log(currency.data.rates);
 
     for (let rate in currency.data.rates) {
@@ -118,5 +238,3 @@ function render() {
         makeComparisonCurrencySelectionOption(convertSelect, rate);
     }
 }
-
-fetchAPIData();
